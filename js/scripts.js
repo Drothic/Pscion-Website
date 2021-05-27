83ca93df12fa419750f51c95302b18856c9dd961
Drothic <devanmreid01@gmail.com>
2021-05-27 02:01:07 -0700

    work on player guide

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('#guideAnchor').click(function(){
+        if ($('#guideBtns').hasClass('collapse')){
+            $('#guideBtns').removeClass('collapse');
+        }
+    });
+    $('#normPlayBtn').click(function(){
+        if ($('#normPlayP').hasClass('collapse')){
+            $('#normPlayP').removeClass('collapse');
+        } else {
+            $('#normPlayP').addClass('collapse');
+        }
+    });
+    $('#combatBtn').click(function(){
+        if ($('#combatP').hasClass('collapse')){
+            $('#combatP').removeClass('collapse');
+        } else {
+            $('#combatP').addClass('collapse');
+        }
+    });
+    
+});
